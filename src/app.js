7617e896febb6d074e14b0ead1c2e98905c8a85c
happyandhappy <greyfrapps@gmail.com>
2018-10-23 05:27:19 +0200

    update 10-23

--- src/app.js
+++ src/app.js
@@ -1,96 +1,102 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://admin:LanaRhoades456@172.104.83.46:27017/eosdb";
+let cnt = 0;
 
-var findActionTraces = function(db, offset){
+// check if current record is existing in collection
+/*var isNanInCollection = async function(db, collection_name, query){    
+    var cursor = null;
+    try{
+        cursor = await db.collection(collection_name).find(query).batchSize(10000);
+    }catch(err){
+        throw err;
+    }
+    if ( cursor != null && await cursor.next() === null){
+        return true;
+    }else return false;
+}*/
+
+var getCoinId  = async function(db, symbol){
+    var query = {
+        "Coin_short" : symbol
+    };
+
+    let coinRecord = await db.collection("coin_list").findOne(query, {});
+    let id = null;    
+    if (coinRecord === null){            
+        id = await db.collection("coin_list").countDocuments() + 1;
+        await db.collection("coin_list").insertOne({
+            "Coin_id"       : id,
+            "Coin_short"    : symbol,
+            "Coin_full"     : '',
+            "Main_chain"    : ''
+        });
+    }else{
+        id = coinRecord.Coin_id;
+    }
+    return id;
+}
+
+var findActionTraces =async function(db, offset){
     console.log("--------------------offset ------------------------");          
     start = new Date().getTime();
-    console.log("start");    
-    db.collection("action_traces").find(
+    let cursor = db.collection("action_traces").find(
         {},
         {_id:0, "trx_id" : 1, "createdAt":1, "receipt":1, "act":1}
     )
     .skip(offset)
-    .limit(10000)
-    .toArray(async (err, result)=>{
-        for ( var i = 0 ; i < result.length ; i++){
-            console.log(offset + i);
-            if (typeof doc.act.data === "object"){
-                if (doc.act.data.quantity != undefined || parseFloat(doc.act.data.quantity) > 0){
-                    if (doc.act.data.memo === undefined) doc.act.data.memo = null;
-                    // console.log('1');
-                    var query = { 
-                        "Txid"            : doc.trx_id, 
-                        "TimeStamp"       : doc.createdAt, 
-                        "Actid"           : doc.receipt.act_digest,
-                        "from_username"   : doc.act.data.from === undefined ? null : doc.act.data.from ,
-                        "to_username"     : doc.act.data.to === undefined ? null : doc.act.data.to
-                    };
-    
-                    // console.log(doc.trx_id);                
-                    var res = null;                    
-                    try{
-                        res = await db.collection("testing_esof_ledger").findOne(query);                        
-                    }catch(err){
-                        throw err;
+    .limit(1000000)
+    .batchSize(100000);
+    console.log(start);
+    for (let doc = await cursor.next(); doc != null; doc = await cursor.next()) {        
+        if (typeof doc.act.data === "object"){
+            if (doc.act.data.quantity != undefined || parseFloat(doc.act.data.quantity) > 0){
+                if (doc.act.data.memo === undefined) doc.act.data.memo = null;
+
+                    let coinId = await getCoinId(db, doc.act.data.quantity.split(' ')[1]);
+                    let coinCollection;
+                    if (coinId === 1){
+                        coinCollection = "eos_price_db";
+                    }else{
+                        coinCollection = "";
                     }
 
-                    if ( res === null){
-                        // console.log('2');
-                        var smartQuery = {"_id.sc_account":doc.act.account, "_id.sc_action":doc.act.name, "_id.sc_memo":doc.act.data.memo};
-                        var smartResult = null;
-                        try{
-                            smartResult = await db.collection("smart_contract_list_update")
-                                .findOne(smartQuery,{ _id : 0, "from_acc_db": 1,"from_acc_cr" : 1, "to_acc_db":1, "to_acc_cr":1 });
-                        }catch(err){
-                            throw err;
-                        }
-                        // console.log('3');
-    
-                        if (smartResult != null){
-                            var data = {
-                                            "TimeStamp"             : doc.createdAt,
-                                            "Txid"                  : doc.trx_id,
-                                            "Actid"                 : doc.receipt.act_digest,
-                                            "from_username"         : doc.act.data.from,
-                                            "from_account_debit"    : smartResult === null ? null: smartResult.from_acc_db,
-                                            "from_account_credit"   : smartResult === null ? null:smartResult.from_acc_cr,
-                                            "to_username"           : doc.act.data.to,
-                                            "to_account_debit"      : smartResult === null ? null:smartResult.to_acc_db,
-                                            "to_account_credit"     : smartResult === null ? null:smartResult.to_acc_cr,
-                                            "amount"                : doc.act.data.quantity === undefined ? null : doc.act.data.quantity.split(' ')[0],
-                                            "currency"              : doc.act.data.quantity === undefined ? '' : doc.act.data.quantity.split(' ')[1],
-                                            "valueinusd"            : doc.act.data.quantity,
-                                        };                        
-                            var date =new Date(data.TimeStamp);
-                            date = new Date(date.getFullYear(),date.getMonth(),date.getDate(),0,0,0,0);
-                            var priceResult = null;
-                            try{
-                                priceResult = await db.collection("eos_price_db").findOne({"date": date});
-                            }catch(err){
-                                throw err;
-                            }           
-                            // console.log('4');
-                            if (priceResult != null){
-                                var amount = Number.isNaN(parseFloat(data.amount) * parseFloat(priceResult['price(USD)'])) ? 0 : parseFloat(data.amount) * parseFloat(priceResult['price(USD)']);
-                                data.valueinusd = amount;
-                                await db.collection("testing_esof_ledger").insertOne(data);
-                                console.log('inserted' + (offset + i));                                                          
-                            }
-                        }                    
+                    var date =new Date(doc.createdAt);
+                    date = new Date(date.getFullYear(), date.getMonth(), date.getDate(),2,0,0,0);                    
+                    try{
+                        priceRecord = await db.collection(coinCollection).findOne({"date": date});
+                    }catch(err){
+                        throw err;
+                    } 
+                                        
+                    if (priceRecord != null){                        
+                        var unit = parseFloat(priceRecord['price(USD)']);
+                        data = {
+                            "Txid"              : doc.trx_id,
+                            "TimeStamp"         : doc.createdAt, 
+                            "Actid"             : doc.receipt.act_digest,
+                            "Sc_account"        : doc.act.account === undefined ? null : doc.act.account ,
+                            "Sc_name"           : doc.act.name === undefined ? null : doc.act.name,
+                            "Sc_memo"           : doc.act.data.memo === undefined ? null : doc.act.data.memo,
+                            "From_username"     : doc.act.data.from,
+                            "To_username"       : doc.act.data.to,
+                            "Amount"            : doc.act.data.quantity.split(' ')[0],
+                            "Currency"          : coinId,
+                            "ValueinUsd"        : parseFloat(doc.act.data.quantity) * unit
+                        };
+                        await db.collection("testing_esof_ledger").insertOne(data);  
+                        // console.log("Insert One");
+                        console.log(cnt++);
                     }
-                } 
             }
         }
-        console.log(new Date().getTime() - start);        
-        findActionTraces(db, offset + 30000);
-    })
+    }
 }
 
 // MongoClient.connect( url , { useNewUrlParser: true },  function(err, client){
 //     if (err)throw err;    
 //     findActionTraces(client.db("eosdb"), 200000000); 
 // });
-
+/*
 var findActionTraces2 = async function (db) {
     let counter = 0
     let cursor = db.collection('action_traces').find(
@@ -167,9 +173,10 @@ var findActionTraces2 = async function (db) {
                 } 
             }
     }
-}
+}*/
 MongoClient.connect( url , { useNewUrlParser: true },  function(err, client){
     if (err)throw err;    
-    findActionTraces2(client.db("eosdb")); 
+    // findActionTraces2(client.db("eosdb"));
+    findActionTraces(client.db("eosdb"),0);
 });
 
--- src/smart.js
+++ src/smart.js
@@ -1,45 +1,29 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://admin:LanaRhoades456@172.104.83.46:27017/eosdb";
 
-var findActionTraces = function(db, offset){
-    console.log('------------------offset------------------');
-    db.collection("action_traces").find(
-        {},
-        {_id:0, "trx_id" : 1, "createdAt":1, "receipt":1, "act":1}        
-    )
-    .skip(offset)
-    .limit(10000)
+var findActionTraces = function(db){    
+    db.collection("smart_contract_list_update").find({})  
     .toArray(async (err, result)=>{
-        for ( var i = 0 ; i < 10000 ; i++){
-            console.log(i + offset);
-            var memo = null;
-            if (typeof result[i].act.data != "object") memo = null;
-            else if (result[i].act.data.memo === undefined) memo = '';
-            else memo = result[i].act.data.memo;
+        if (err) throw err;
+        console.log('------------------offset------------------');
+        for ( var i = 0 ; i < result.length ; i++){            
+            var data = {
+                "_id"           : result[i]._id,
+                "To_event1_id"  : 0,
+                "To_event2_id"  : 0,
+                "From_event1_id": 0,
+                "From_event2_id": 0
+            }
+            console.log(data);
             try{                
-                await db.collection("smart_contract_list_test").insertOne({
-                    "_id":{
-                        "sc_account" : result[i].act.account,
-                        "sc_action":result[i].act.name, 
-                        "sc_memo":memo,    
-                    },
-                    "is_financial" : '1',
-                    "is_social"    :'',
-                    "is_politica" : '',
-                    "is_marketing" : "",
-                    "from_acc_db" : "",
-                    "from_acc_cr":"",
-                    "to_acc_db" : "",
-                    "to_acc_cr" : ""
-                });
+                // await db.collection("smart_contract_list").insertOne(data);
                 console.log("inserted one")
             }catch(err){}
-        }
-        findActionTraces(db, offset + 10000);
+        }        
     });
 }
 
 MongoClient.connect( url , { useNewUrlParser: true },  function(err, client){
     if (err)throw err;
-    findActionTraces(client.db("eosdb"), 0); 
+    findActionTraces(client.db("eosdb")); 
 });
